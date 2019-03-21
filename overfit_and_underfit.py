0145dbcb66ff7ca6ceeee059d8ad1bfcc6dbe223
Vantis <vantis.zhang@voyageone.cn>
2019-03-21 19:00:00 +0800

    https://www.tensorflow.org/tutorials/keras/overfit_and_underfit

--- overfit_and_underfit.py
+++ overfit_and_underfit.py
@@ -38,3 +38,69 @@ baseline_model.compile(optimizer='adam',
                        metrics=['accuracy', 'binary_crossentropy'])
 
 baseline_model.summary()
+
+baseline_history = baseline_model.fit(train_data,
+                                      train_labels,
+                                      epochs=20,
+                                      batch_size=512,
+                                      validation_data=(test_data, test_labels),
+                                      verbose=2)
+
+smaller_model = keras.Sequential([
+    keras.layers.Dense(4, activation=tf.nn.relu, input_shape=(NUM_WORDS,)),
+    keras.layers.Dense(4, activation=tf.nn.relu),
+    keras.layers.Dense(1, activation=tf.nn.sigmoid)
+])
+
+smaller_model.compile(optimizer='adam',
+                      loss='binary_crossentropy',
+                      metrics=['accuracy', 'binary_crossentropy'])
+
+smaller_model.summary()
+
+smaller_history = smaller_model.fit(train_data,
+                                    train_labels,
+                                    epochs=20,
+                                    batch_size=512,
+                                    validation_data=(test_data, test_labels),
+                                    verbose=2)
+
+bigger_model = keras.models.Sequential([
+    keras.layers.Dense(512, activation=tf.nn.relu, input_shape=(NUM_WORDS,)),
+    keras.layers.Dense(512, activation=tf.nn.relu),
+    keras.layers.Dense(1, activation=tf.nn.sigmoid)
+])
+
+bigger_model.compile(optimizer='adam',
+                     loss='binary_crossentropy',
+                     metrics=['accuracy', 'binary_crossentropy'])
+
+bigger_model.summary()
+
+bigger_history = bigger_model.fit(train_data,
+                                  train_labels,
+                                  epochs=20,
+                                  batch_size=512,
+                                  validation_data=(test_data, test_labels),
+                                  verbose=2)
+
+
+def plot_history(histories, key='binary_crossentropy'):
+    plt.figure(figsize=(16, 10))
+
+    for name, history in histories:
+        val = plt.plot(history.epoch, history.history['val_' + key], '--', label=name.title() + ' Val')
+        plt.plot(history.epoch, history.history[key], color=val[0].get_color(), label=name.title() + ' Train')
+
+        plt.xlabel('Epochs')
+        plt.ylabel(key.replace('_', ' ').title())
+        plt.legend()
+
+        plt.xlim([0, max(history.epoch)])
+
+
+plot_history([
+    ('baseline', baseline_history),
+    ('smaller', smaller_history),
+    ('bigger', bigger_history)
+])
---
+++ readme.md
@@ -0,0 +1,8 @@
+# TensorFlow
+1. Learn and use ML
+    1. Overview
+    2. [Basic classification](basic_classification.py)
+    3. [Text classification](basic_text_classification.py)
+    4. [Regression](basic_regression.py)
+    5. [Overfitting and underfitting](overfit_and_underfit.py)
+ 2. Research and experimentation
