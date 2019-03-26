eede1eda4ac1d49dabb7d1df70777f775527bd85
Vantis <vantis.zhang@voyageone.cn>
2019-03-26 17:07:04 +0800

    https://www.tensorflow.org/tutorials/eager/eager_basics

--- eager_basics.py
+++ eager_basics.py
@@ -36,3 +36,30 @@ print(tf.test.is_gpu_available())
 
 print("Is the Tensor on GPU #0: "),
 print(x.device.endswith('GPU:0'))
+
+import time
+
+
+def time_matmul(x):
+    start = time.time()
+    for loop in range(10):
+        tf.matmul(x, x)
+
+    result = time.time() - start
+
+    print("10 loops: {:0.2f}ms".format(1000 * result))
+
+
+# Force execution on CPU
+print("On CPU:")
+with tf.device("CPU:0"):
+    x = tf.random_uniform([1000, 1000])
+    assert x.device.endswith("CPU:0")
+    time_matmul(x)
+
+# Force execution on GPU #0 if available
+if tf.test.is_gpu_available():
+    with tf.device("GPU:0"):
+        x = tf.random_uniform([1000, 1000])
+        assert x.device.endswith("GPU:0")
+        time_matmul(x)
